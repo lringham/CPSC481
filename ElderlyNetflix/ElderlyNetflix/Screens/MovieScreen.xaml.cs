803f40ddb73cbe63ffcd7d114727c68904dcadea
nili.na1989@gmail.com <nili.na1989@gmail.com>
2015-03-12 09:49:19 -0600

    3 pages

---
+++ ElderlyNetflix/ElderlyNetflix/Screens/MovieScreen.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ElderlyNetflix.Code;
+
+namespace ElderlyNetflix.Screens
+{
+    /// <summary>
+    /// Interaction logic for MovieScreen.xaml
+    /// </summary>
+    public partial class MovieScreen : UserControl
+    {
+        
+        public  MovieScreen()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Navigator.navigateBack();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Navigator.navigateAndClearStack(new MainScreen());
+        }
+}
+
+
+
+
+
+        }
+    
